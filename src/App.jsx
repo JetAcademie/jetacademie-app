c65132c3a55ae089e839684e1cb97a77a24c0a7a
senesuzun <senesuzun@gmail.com>
2024-11-26 14:17:37 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Navbar from "./pages/HomePage/Navbar/Navbar";
 import InfoSection from "./pages/HomePage/info-section/InfoSection.jsx";
+import Footer from "./pages/HomePage/footer/footer.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 
@@ -9,6 +11,8 @@ function App() {
     <>
       <Navbar />
         <InfoSection />
+        <Footer />
+
     </>
   );
 }
---
+++ src/pages/HomePage/footer/footer.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-[#16407a] text-white py-10">
+            <div className="container mx-auto px-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+                    <div>
+                        <h5 className="text-lg font-bold mb-4 border-b-2 border-gray-300 pb-2">
+                            Adres
+                        </h5>
+                        <p className="text-gray-300">
+                            Kunstlaan 3,
+                            <br />
+                            1210 Brussel
+                        </p>
+                    </div>
+
+                    <div>
+                        <h5 className="text-lg font-bold mb-4 border-b-2 border-gray-300 pb-2">
+                            Contact
+                        </h5>
+                        <p className="text-gray-300">
+                            info@vzwjet.be
+                            <br />
+                            +32 488 60 36 98
+                        </p>
+                    </div>
+
+                    <div>
+                        <h5 className="text-lg font-bold mb-4 border-b-2 border-gray-300 pb-2">
+                            Regio-mailadressen:
+                        </h5>
+                        <ul className="text-gray-300 space-y-2">
+                            <li>antwerpen@vzwjet.be</li>
+                            <li>limburg@vzwjet.be</li>
+                            <li>gent@vzwjet.be</li>
+                            <li>brussel@vzwjet.be</li>
+                            <li>leuven@vzwjet.be</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h5 className="text-lg font-bold mb-4 border-b-2 border-gray-300 pb-2 text-center">
+                            Bizi Takip Edin
+                        </h5>
+                        <div className="flex justify-center space-x-6">
+                            <a
+                                href="https://www.instagram.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-gray-300 hover:text-gray-100 transition duration-300"
+                            >
+                                <i className="fab fa-instagram fa-2x"></i>
+                            </a>
+                            <a
+                                href="https://www.youtube.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-gray-300 hover:text-gray-100 transition duration-300"
+                            >
+                                <i className="fab fa-youtube fa-2x"></i>
+                            </a>
+                            <a
+                                href="https://www.facebook.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-gray-300 hover:text-gray-100 transition duration-300"
+                            >
+                                <i className="fab fa-facebook fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="border-t border-gray-500 mt-8 pt-6 flex flex-col md:flex-row items-center justify-between text-gray-300">
+                    <div className="text-center md:text-left text-sm">
+                        &copy; 2024. Alle rechten voorbehouden aan{" "}
+                        <a
+                            href="#"
+                            className="text-green-400 hover:text-green-300 transition duration-300"
+                        >
+                            vzw JET
+                        </a>
+                    </div>
+                    <div className="my-4 md:my-0">
+                        <img
+                            src="https://vzwjet.be/wp-content/uploads/2024/05/vzw-jet-logo.png"
+                            alt="Logo"
+                            className="max-w-[80px] mx-auto"
+                        />
+                    </div>
+                    <div className="text-center md:text-right text-sm">
+                        Designed by{" "}
+                        <span className="font-semibold text-green-400 hover:text-green-300 transition duration-300">
+              JET
+            </span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
