a10188cc9062591639ab76dec1172d496f911285
senesuzun <senesuzun@gmail.com>
2024-11-27 14:38:42 +0100

    motivation section

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -2,12 +2,14 @@ import Navbar from "./Navbar/Navbar";
 import InfoSection from "./info-section/InfoSection";
 import Footer from "./footer/Footer";
 import MissionSection from "./mission-section/MissionSection.jsx";
+import MotivationSection from "./MotivationSection/MotivationSection.jsx";
 
 const Homepage = () => {
     return (
         <div>
             <Navbar />
             <InfoSection />
+            <MotivationSection/>
             <MissionSection />
             <Footer />
         </div>
---
+++ src/pages/HomePage/MotivationSection/MotivationSection.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { quotesData } from "../../../data/data.js";
+
+const MotivationSection = () => {
+    const [currentQuote, setCurrentQuote] = useState(quotesData[0]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * quotesData.length);
+            setCurrentQuote(quotesData[randomIndex]);
+        }, 10000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <section className="py-12 bg-gradient-to-br from-indigo-600 via-blue-500 to-purple-500 text-white rounded-xl shadow-xl mx-6 my-8">
+            <div className="container mx-auto px-6">
+                <div className="max-w-2xl mx-auto bg-white bg-opacity-10 backdrop-blur-md p-6 rounded-lg shadow-md">
+                    <h2 className="text-2xl font-bold mb-4 text-gray-100 tracking-wide">A Journey from Words to Wisdom</h2>
+                    <p className="text-lg italic font-medium text-gray-50 mb-4">
+                        {`"${currentQuote.text}"`}
+                    </p>
+                    <p className="text-sm font-medium text-gray-300">- {currentQuote.author}</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MotivationSection;
