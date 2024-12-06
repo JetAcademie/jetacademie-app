c3176701a3e518808394444615b6ded7fb54b626
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2024-12-06 23:58:30 +0100

    `mailto:` used for mails

--- src/components/LocationCard.jsx
+++ src/components/LocationCard.jsx
@@ -2,13 +2,16 @@ import PropTypes from "prop-types";
 
 const LocationCard = ({ icon, title, email }) => {
     return (
-        <div className="bg-gradient-to-br from-white to-gray-100 rounded-xl shadow-lg p-6 border border-gray-300 transform hover:scale-105 hover:shadow-2xl transition-transform duration-300">
+        <a
+            href={`mailto:${email}`}
+            className="block bg-gradient-to-br from-white to-gray-100 rounded-xl shadow-lg p-6 border border-gray-300 transform hover:scale-105 hover:shadow-2xl transition-transform duration-300"
+        >
             <h5 className="text-xl font-bold flex items-center mb-4 text-gray-800">
                 <i className={`${icon} text-blue-500 mr-3`}></i>
                 {title}
             </h5>
             <p className="text-gray-600">E-posta: {email}</p>
-        </div>
+        </a>
     );
 };
 
